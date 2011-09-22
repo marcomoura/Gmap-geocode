2f34c01be5409d598ee3eba46d28501fe578758b
Marco Moura <email@marcomoura.com>
2011-09-22 08:42:27 -0300

    fix .watchr

--- .watchr.rb
+++ .watchr.rb
@@ -2,6 +2,6 @@ watch('(.*).php') { |m| code_changed(m[0]) }
 watch('./Tests/(.*).php') { |m| code_changed(m[0]) }
 
 def code_changed(file)
-    system( "phpunit --colors Tests/" )
+    system( "phpunit --colors test/" )
 end
 
